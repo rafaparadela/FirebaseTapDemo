2bee1e12bbe71c1d4b94dbade95511a7da10be4c
Rafa Paradela <rafaparadela@gmail.com>
2014-07-13 18:37:10 +0200

    Initial Commit

---
+++ DemoFirebaseDrag/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoFirebaseDrag
+//
+//  Created by Rafa Paradela on 13/07/14.
+//  Copyright (c) 2014 rafaparadela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DemoFirebaseDrag/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoFirebaseDrag
+//
+//  Created by Rafa Paradela on 13/07/14.
+//  Copyright (c) 2014 rafaparadela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
